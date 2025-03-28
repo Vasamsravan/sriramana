80542fc50cbac90fc880abc136aaa9ca8331827b
Dreamyplayer <saikumarchinna123@gmai.com>
2025-03-28 11:30:10 +0530

    fix: needless font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Sri Ramana Events",
+  title: 'Sri Ramana Events',
   description: `Creating Unforgettable Moments for Your Special Events
 Exquisite decorations and flawless event management for weddings, birthdays, anniversaries, and more.`,
 };
-
-<link rel="icon" href="/Final SR Logo-03-01 1 1 (vectorized).png" type="image/png" sizes="32x32" />
 
 export default function RootLayout({
   children,
@@ -26,12 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={`scroll-smooth`}>{children}</body>
     </html>
   );
 }
